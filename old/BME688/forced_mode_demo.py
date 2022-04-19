1c08824ef63ee54cbde4f7f0cab642767b7c5e22
Theo Mouyen <Theo.Mouyen@uni-bayreuth.de>
2022-04-19 16:08:32 +0200

    Second commit

--- old/BME688/forced_mode_demo.py
+++
@@ -1,9 +0,0 @@
-import bme
-
-bme.init("/dev/i2c-1", 119)
-bme.set_timezone("TZ=Europe/London")   # other options include "TZ=Asia/Singapore" or "TZ=US/Denver"
-bme.forced_mode()
-print(" year mon day h min sec  Tmp degC            Prs Pa         Hum %rH           GsR Ohm   Status")
-data = bme.get_data()
-print(data)
-print("Installation succeeded")
